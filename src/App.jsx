63aabada5aebfbd1ff8c5c449925a8d0e7f85672
ashwanikunal <technicalthingz031@gmail.com>
2025-10-20 23:45:20 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     
+    <h1 className="text-3xl font-bold underline">
+      Note Taking App
+    </h1>
+    <div className='Px-4 '>
+
+    </div>
+    </>
+  )
+}
+
+export default App
