fcec5cee60b72a13d6adaec8f0beee514beb2368
ashwanikunal <technicalthingz031@gmail.com>
2025-10-21 11:58:43 +0530

    .

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Header( {onOpenNew,query,setQuery}) {
     return(
         <header className='flex items-center justify-between p-4 bg-gray-50'>
-        <h1 className='text-xl font-semibold'>Keep Clone</h1>
+        <h1 className='text-xl font-semibold'>Note Taking App</h1>
         <div className='flex gap-2 items-center'>
          <input 
          value ={query}
@@ -12,7 +12,7 @@ export default function Header( {onOpenNew,query,setQuery}) {
          className = 'px-3 py-2 border rounded-md outline-none'/>
          <button onClick={onOpenNew}
          className='px-4 py-2 bg-blue-600 text-white rounded-md'>
-            n
+            new
          </button>
         </div>
         </header>
